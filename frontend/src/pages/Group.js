f5db2b28da9b32d5a40932d4996772068c6f39a7
arttu aalto <t1aaar01@students.oamk.fi>
2024-04-05 19:34:14 +0300

    Group page

--- frontend/src/pages/Group.js
+++ frontend/src/pages/Group.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import './group.css'
+import React, { useEffect, useState } from 'react';
+import './group.css';
+import { Link } from 'react-router-dom';
 
 export default function Group() {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/group/allGroups');
+        if (!response.ok) {
+          throw new Error('Failed to fetch groups from the database');
+        }
+        const data = await response.json();
+        setGroups(data);
+      } catch (error) {
+        console.error('Failed to fetch groups from the database', error);
+      }
+    };
+
+    fetchGroups();
+  }, []);
+
   return (
-    <div>Group</div>
-  )
+    <div>
+      <section className='allGroups'>
+        <p>Groups</p>
+        {groups.map(group => (
+          <Link>Nimi:{group.groupname}{group.groupdetails}</Link>
+        ))}
+      </section>
+      <section className='createGroup'>
+        <button>Create a group</button>
+      </section>
+    </div>
+  );
 }
