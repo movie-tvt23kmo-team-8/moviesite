1ac1c7e0dcd5ce1e7574bd3bb5b292252afed898
Ville-A <149409053+Ville-A@users.noreply.github.com>
2024-04-11 13:54:18 +0300

    frontend for login and register modified

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import {jwtToken} from '../components/Signals';
-import { useUser } from "../context/useUser";
+import './login.css'
 
 export default function Login(){
-const { setUser } = useUser()
 const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
+const [error, setError] = useState('');
 const navigate = useNavigate()
 
 function handleLogin(event) {
@@ -21,21 +21,24 @@ function handleLogin(event) {
         navigate("/");
         window.location.reload();
       } else {
-        console.error("JWT token not found in response");
+        console.error("JWT token not found");
+        setError("Error loggin in");
       }
     })
     .catch(error => {
       console.error("Login failed:", error.message);
+      setError("Login failed. Please check your credentials.");
     });
 }
 
 return (
-      <div>
+      <div id="login-form">
         <h2>Kirjaudu</h2>
         <form onSubmit={handleLogin}>
           <input type="text" name="username" placeholder="Käyttäjänimi" value={username} onChange={e => setUsername(e.target.value)} autoComplete="username"/><br/>
           <input type="password" name="password" placeholder="Salasana" value={password} onChange={e => setPassword(e.target.value)} autoComplete="current-password"/><br/>
           <button type="submit">Kirjaudu</button>
         </form>
+        {error && <p>{error}</p>}
       </div>
 )}
