30780fcac359d84c9682ecf79f7d72939d4efce4
Ville-A <149409053+Ville-A@users.noreply.github.com>
2024-04-15 12:37:15 +0300

    Modified register. Made so after user registers they login aswell

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { loginUser } from "../context/LoginUtil";
 import './register.css';
 import axios from "axios";
 import Button from '@mui/material/Button';
@@ -8,30 +9,44 @@ export default function Register() {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
   function handleRegister(event) {
     event.preventDefault();
     
-    if (!username || !password) {
+    if (!username || !password || !confirmPassword) {
       setError('Täytä kaikki pakolliset kentät.');
       return;
     }
-  
+
+    if (password !== confirmPassword) {
+      setError('Salasanat eivät täsmää.');
+      return;
+    }
+
     axios.post('/register', {username, password})
       .then(resp => {
-        navigate("/");
-        window.location.reload();
+        loginUser(username, password)
+      .then(success => {
+        if (success) {
+          navigate("/");
+          window.location.reload();
+        } else {
+          setError("Error logging in. Please try again.");
+        }
+        });
       })
-      .catch(error => {
-        console.error("Register failed:", error.message);
-        if (error.response.status === 400) {
+
+    .catch(error => {
+      console.error("Register failed:", error.message);
+        if (error.response && error.response.status === 400) {
           setError(error.response.data.error);
         } else {
           setError("Rekisteröityminen epäonnistui. Yritä uudelleen myöhemmin.");
         }
-      });
+    });
   }
 
   return (
@@ -42,11 +57,11 @@ export default function Register() {
         <input type="text" name="username" placeholder="Käyttäjänimi" value={username} onChange={e => setUsername(e.target.value)} required /> <br/>
         <label>Salasana</label>
         <input type="password" name="password" placeholder="Salasana" value={password} onChange={e => setPassword(e.target.value)} required /> <br/>
+        <label>Vahvista salasana</label>
+        <input type="password" name="confirmPassword" placeholder="Vahvista salasana" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required /> <br/>
         <Button className='register-button' type="submit" variant="contained">Rekisteröidy</Button>
       </form>
       {error && <p>{error}</p>}
     </div>
   );
 }
-
-
