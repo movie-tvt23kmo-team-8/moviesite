585da5fc993fccd0f8dbe37ac44ecc6577538616
Ereride <143874893+Ereride@users.noreply.github.com>
2024-04-16 21:51:59 +0300

    popupwindow
    homepage elokuvat suomeksi mutta sarjat vielä englanniksi

--- frontend/src/components/Mediapanel.js
+++ frontend/src/components/Mediapanel.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import PopupWindow from './Popupwindow'; 
 import Box from '@mui/material/Box'; // Tuodaan MUI-kirjaston Box-komponentti
 import Rating from '@mui/material/Rating'; // Tuodaan MUI-kirjaston Rating-komponentti
 import './mediapanel.css';
@@ -8,9 +9,10 @@ const MediaPanel = ({ mediaType }) => { // Luodaan MediaPanel-komponentti ja ann
     const [mediaItems, setMediaItems] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0); // Tilamuuttuja nykyiselle indeksille media-alkiolistassa
     const [isHovered, setIsHovered] = useState(false); // Tilamuuttuja hiiren osoittimen tilalle
+    const [selectedMediaItem, setSelectedMediaItem] = useState(null); // State to store the selected media item
 
     useEffect(() => { // Effekti, joka käynnistyy kun mediatyyppi muuttuu
-        const fetchMediaItems = async () => { // Haetaan mediatiedot TMDB API:sta
+        const fetchMediaItems = async () => {
             try {
                 const response = await axios.get(`http://localhost:3001/tmdb/popular-${mediaType}`);
                 setMediaItems(response.data);
@@ -19,10 +21,10 @@ const MediaPanel = ({ mediaType }) => { // Luodaan MediaPanel-komponentti ja ann
             }
         };
 
-        fetchMediaItems(); // Kutsutaan funktiota heti komponentin latauksen yhteydessä
-    }, [mediaType]); // Riippuvuuslistana on mediatyyppi
+        fetchMediaItems();  // Kutsutaan funktiota heti komponentin latauksen yhteydessä
+    }, [mediaType]);  // Riippuvuuslistana on mediatyyppi
 
-    useEffect(() => { // Effekti, joka käynnistyy kun media-alkiot tai hiiren tila muuttuvat
+    useEffect(() => {  // Effekti, joka käynnistyy kun media-alkiot tai hiiren tila muuttuvat
         const interval = setInterval(() => { // Asetetaan ajastin automaattista indeksin päivitystä varten
             if (!isHovered) { // Tarkistetaan, ettei hiiri ole kohdalla
                 setCurrentIndex((prevIndex) => (prevIndex + 1) % mediaItems.length); // Päivitetään indeksiä
@@ -33,26 +35,26 @@ const MediaPanel = ({ mediaType }) => { // Luodaan MediaPanel-komponentti ja ann
     }, [mediaItems, isHovered]); // Riippuvuuslistana on media-alkiot ja hiiren tila
 
     const handleMouseEnter = () => { // Käsittelijä hiiren osoittimen tuloon alueelle
-        setIsHovered(true); // 
+        setIsHovered(true);
     };
 
     const handleMouseLeave = () => { // Käsittelijä hiiren osoittimen poistumiseen alueelta
         setIsHovered(false);
     };
 
-     // Laske kuvien määrä sivulla dynaamisesti näytön leveyden perusteella
-     const calculateItemsPerPage = () => {
+    // Laske kuvien määrä sivulla dynaamisesti näytön leveyden perusteella
+    const calculateItemsPerPage = () => {
         const screenWidth = window.innerWidth;
         let itemsPerPage;
         if (screenWidth >= 1200) {
             itemsPerPage = 10;
-        } else if (screenWidth >= 1200) {
+        } else if (screenWidth >= 1000) {
             itemsPerPage = 8;
         } else if (screenWidth >= 800) {
             itemsPerPage = 6;
         } else if (screenWidth >= 400) {
             itemsPerPage = 4;
-        }else {
+        } else {
             itemsPerPage = 2;
         }
         return itemsPerPage;
@@ -63,19 +65,29 @@ const MediaPanel = ({ mediaType }) => { // Luodaan MediaPanel-komponentti ja ann
     const getMediaIndex = (index) => { // Funktio, joka varmistaa, että indeksi pysyy media-alojen määrän sisällä
         const length = mediaItems.length;
         if (length === 0) return null; // Palautetaan 'null' jos media-alkiolista on tyhjä
-        return index % length; // Palautetaan oikea indeksi media-alkiolistasta
+        return index % length;  // Palautetaan oikea indeksi media-alkiolistasta
+    };
+
+    // käsittelee kuvan käsittelyn
+    const handleMediaItemClick = (mediaItem) => {
+        setSelectedMediaItem(mediaItem);
+    };
+
+    // Sulkee popup ikkunan
+    const handleClosePopup = () => {
+        setSelectedMediaItem(null);
     };
 
     return (
-        <div className='container-media' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> {/* Container-divi, joka reagoi hiiren tilaan */}
+        <div className='container-media' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>  {/* Container-divi, joka reagoi hiiren tilaan */}
             <div className="poster-gallery">
-                {[...Array(itemsPerPage).keys()].map((offset) => { // Karttafunktio, joka luo media-alkiot näytettäväksi
-                    const mediaIndex = getMediaIndex(currentIndex + offset); // Haetaan oikea indeksi media-alkiolle
+                {[...Array(itemsPerPage).keys()].map((offset) => {  // Karttafunktio, joka luo media-alkiot näytettäväksi
+                    const mediaIndex = getMediaIndex(currentIndex + offset);  // Haetaan oikea indeksi media-alkiolle
                     if (mediaIndex !== null) { // Tarkistetaan, että indeksi on kelvollinen
                         const mediaItem = mediaItems[mediaIndex]; // Haetaan media-alkio indeksillä
 
                         return (
-                            <div key={mediaItem.id} className="media-container">
+                            <div key={mediaItem.id} className="media-container" onClick={() => handleMediaItemClick(mediaItem)}>
                                 <div className="media-poster-container">
                                     <img className="media-poster" src={`https://image.tmdb.org/t/p/w500/${mediaItem.poster_path}`} alt={mediaItem.title} />
                                     <Box>
@@ -89,22 +101,25 @@ const MediaPanel = ({ mediaType }) => { // Luodaan MediaPanel-komponentti ja ann
                             </div>
                         );
                     }
-                    return null; // Palautetaan 'null', jos media-alkio on tyhjä
+                    return null;  // Palautetaan 'null', jos media-alkio on tyhjä
                 })}
             </div>
+            {/* Popup window component */}
+            {selectedMediaItem && (
+                <PopupWindow mediaItem={selectedMediaItem} onClose={handleClosePopup} />
+            )}
         </div>
     );
 };
 
-// BasicRating-komponentti tähti-arvostelun näyttämiseksi
-const BasicRating = ({ value }) => { // Tähti-arvostelukomponentti
+const BasicRating = ({ value }) => {
     return (
-          <div className='rating'>
+        <div className='rating'>
             <Box
                 sx={{
                     '& > legend': { mt: 2 },
                     '& .MuiRating-icon': { // Selecting the icon of the Rating component
-                        fontSize: '1vw', // Adjust the font size using viewport width units
+                        fontSize: '1vw',  // Adjust the font size using viewport width units
                     },
                 }}
             >
@@ -114,4 +129,4 @@ const BasicRating = ({ value }) => { // Tähti-arvostelukomponentti
     );
 };
 
-export default MediaPanel; // Viedään MediaPanel-komponentti käytettäväksi
+export default MediaPanel;
---
+++ frontend/src/components/Popupwindow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './popupwindow.css'; // Import the CSS file for styling
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+
+const Popupwindow = ({ mediaItem, onClose }) => {
+    return (
+        <div className="popup-overlay"> {/* Container to cover the entire page */}
+            <div className="popup-window">
+                <div className="popup-content">
+                    <img src={`https://image.tmdb.org/t/p/w400/${mediaItem.poster_path}`} alt={mediaItem.title} />
+                    <div className='popup-detail'>
+                        <h1 className='movietitle'>{mediaItem.title}</h1>
+                        {mediaItem.original_name && <h1 className='seriesTitle'>{mediaItem.original_name}</h1>}
+                        <p className='mediadetail'>{mediaItem.overview}</p>
+                        <p className='mediadetail'>Release date: {mediaItem.release_date}</p>
+                    </div>
+                    <Box>
+                        <BasicRating value={mediaItem.vote_average / 2} />
+                    </Box>
+                </div>
+                <button onClick={onClose}>Close</button>
+                <i className="popupIcon-heart fa-solid fa-heart-circle-plus"></i>
+                <i className="popupIcon-group fa-solid fa-users-rectangle"></i>
+            </div>
+        </div>
+    );
+};
+
+const BasicRating = ({ value }) => {
+    return (
+        <div className='rating'>
+            <Box
+                sx={{
+                    '& > legend': { mt: 2 },
+                    '& .MuiRating-icon': {
+                        fontSize: '1vw',
+                    },
+                }}
+            >
+                <Rating className="read-only" value={value} readOnly />
+            </Box>
+        </div>
+    );
+};
+
+export default Popupwindow;
