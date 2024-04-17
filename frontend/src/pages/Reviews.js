50b732639db4ba97392d4905cd85ecdcbdf49a6a
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-04-17 23:00:54 +0300

    Kaikkien arvostelujen nouto onnistuu backendistä. Arvostelun lisääminen postmanilla toimii

--- frontend/src/pages/Reviews.js
+++ frontend/src/pages/Reviews.js
@@ -1,8 +1,37 @@
-import React from 'react'
-import './reviews.css'
+import React, { useEffect, useState } from 'react';
+import './reviews.css';
 
 export default function Reviews() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/review/allReviews');
+        if (!response.ok) {
+          throw new Error('Failed to fetch reviews from database');
+        }
+        const data = await response.json();
+        setReviews(data);
+      } catch (error) {
+        console.error('Failed to fetch reviews from database', error);
+      }
+    };
+    fetchReviews();
+  }, []);
+
   return (
-    <div>Reviews</div>
-  )
+    <div className='review-container'>
+      <h1>Reviews</h1>
+      <div className='review-card'>
+        {reviews.map((review, index) => (
+          <div key={index}>
+            <p>Arvostelija: {review.idaccount}</p>
+            <p>MdbData: {review.mdbdata}</p>
+            <p>Review: {review.review}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
