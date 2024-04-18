2519793db774d5a185497106178472268c9ea226
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-04-18 19:25:00 +0300

    Lisätty moviedatabasen kuvahaku arvosteluille.

--- frontend/src/pages/Reviews.js
+++ frontend/src/pages/Reviews.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './reviews.css';
 
 export default function Reviews() {
@@ -12,14 +13,36 @@ export default function Reviews() {
           throw new Error('Failed to fetch reviews from database');
         }
         const data = await response.json();
-        setReviews(data);
+
+        // Haetaan TMDB:stä kullekin arvostelulle vastaava posterin osoite
+        const reviewsWithPosters = await Promise.all(data.map(async (review) => {
+          try {
+            const tmdbResponse = await axios.get(`http://localhost:3001/tmdb/poster?id=${review.mdbdata}`);
+
+            // Tarkistetaan, onko vastauksessa posterin URL
+            if (tmdbResponse.data && tmdbResponse.data.poster_path) {
+              return {
+                ...review,
+                posterUrl: tmdbResponse.data.poster_path,
+                title: tmdbResponse.data.title || review.title // Käytä oletusotsikkoa, jos vastauksessa ei ole otsikkoa
+              };
+            } else {
+              // Palautetaan alkuperäinen arvostelu ilman posterin osoitetta, jos haku epäonnistui
+              return { ...review, title: review.title }; // Käytä alkuperäistä otsikkoa
+            }
+          } catch (error) {
+            console.error('Failed to fetch poster from TMDB', error);
+            return { ...review }; // Palauta alkuperäinen arvostelu virhetilanteessa
+          }
+        }));
+
+        setReviews(reviewsWithPosters);
       } catch (error) {
         console.error('Failed to fetch reviews from database', error);
       }
     };
     fetchReviews();
   }, []);
-
 
   return (
     <div className='review-container'>
@@ -28,8 +51,10 @@ export default function Reviews() {
         {reviews.map((review, index) => (
           <div key={index}>
             <p>Arvostelija: {review.username}</p>
-            <p>MdbData: {review.mdbdata}</p>
+            <p>Elokuvan nimi: {review.title}</p>
             <p>Review: {review.review}</p>
+            {review.posterUrl && <img src={`https://image.tmdb.org/t/p/original${review.posterUrl}`} alt="Movie Poster" />}
+            <p>Tässä on linkki elokuvan posterinsivuille: https://image.tmdb.org/t/p/original${review.posterUrl}</p>
           </div>
         ))}
       </div>
