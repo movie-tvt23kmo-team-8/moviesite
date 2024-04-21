7b3415388175044d21d2a6acfbbaa50ff7e79931
Ereride <143874893+Ereride@users.noreply.github.com>
2024-04-22 01:06:44 +0300

    Update Topbar.js

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -1,11 +1,51 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './topbar.css'
 import { Link } from 'react-router-dom'
 import { jwtToken } from '../components/Signals';
 import { useUser } from '../context/useUser';
 
 export default function TopBar() {
+  const [username, setUsername] = useState('');
+  const [imageid, setImageId] = useState('');
+  const [loading, setLoading] = useState(true); // State to track loading status
+
+
   const user = jwtToken.value.length !== 0;
+
+  useEffect(() => {
+    // Fetch user data after 5 seconds
+    const fetchData = setTimeout(() => {
+      axios.get('http://localhost:3001/users/personal', {
+        headers: {
+          Authorization: `Bearer ${jwtToken.value}`
+        }
+      })
+        .then(response => {
+          setUsername(response.data.username);
+          setImageId(response.data.imageid);
+          setLoading(false); // Set loading to false after data is fetched
+        })
+        .catch(error => {
+          console.error('Error fetching username and imageid:', error);
+          setLoading(false); // Set loading to false even if there's an error
+        });
+    }, 10);
+
+    // Clear the timer when component unmounts
+    return () => clearTimeout(fetchData);
+  }, []);
+
+  useEffect(() => {
+    console.log('Image ID:', imageid);
+    console.log('Image Source:', `../img/avatar/${imageid}.png`);
+  }, [imageid]);
+
+  // Render loading message while data is being fetched
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='top'>
       <div className='topLeft'>
@@ -42,7 +82,14 @@ export default function TopBar() {
             (
               <>
                 <ul className='topLogout'><li className='topLogoutItem'>
-                  <Link to="/profile"><img className='topRightImg' src={require('../img/logo.png')} alt="topimg" /></Link>
+                  <Link to="/profile">{imageid && (
+          <img
+            className='topRightImg'
+            src={require(`../img/avatar/${imageid}.png`)}
+            alt="topimg"
+          />
+        )}
+</Link>
 
                 </li><li className='topLogoutItem'>
                     <Link className='link logout' to="/logout">Kirjaudu ulos</Link>
