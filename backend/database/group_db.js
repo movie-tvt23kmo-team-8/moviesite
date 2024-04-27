a56b523d9520e2f76236f1a7b31dc07959da2729
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-04-27 17:24:35 +0300

    Backend tehty

--- backend/database/group_db.js
+++ backend/database/group_db.js
@@ -6,7 +6,9 @@ const sql = {
     ADDGROUP: 'INSERT INTO "group" (idaccount, groupname, groupdetails) VALUES ($1, $2, $3)',
     GET_GROUPS: 'SELECT * FROM "group"',
     GET_GROUP_ID: 'SELECT (idgroup) FROM "group" WHERE idaccount = $1 ORDER BY "idgroup" DESC LIMIT 1',
-    GET_GROUP_ID_BY_NAME: 'SELECT (idgroup) FROM "group" WHERE groupname= $1'
+    GET_GROUP_ID_BY_NAME: 'SELECT (idgroup) FROM "group" WHERE groupname= $1',
+    GET_GROUP_MEMBERS: 'SELECT groupmember.*, account.username AS username FROM groupmember JOIN  account ON groupmember.idaccount = account.idaccount WHERE idgroup=$1',
+    GET_GROUP_DETAILS: 'SELECT * FROM "group" WHERE idgroup=$1'
 }
 
 async function addGroup(idaccount, groupname, groupdetails) {
@@ -31,4 +33,17 @@ async function getGroupIDbyGroupname(groupname) {
     return result.rows[0];
 }
 
-module.exports = {addGroup, getGroups, getGroupID, getGroupIDbyGroupname};
+async function getGroupMembers(idgroup) {
+    const result = await pgPool.query(sql.GET_GROUP_MEMBERS, [idgroup])
+    console.log(result.rows);
+    return result.rows;
+}
+
+async function getGroupDetails(idgroup) {
+    const result = await pgPool.query(sql.GET_GROUP_DETAILS, [idgroup])
+    console.log(result.rows);
+    return result.rows;
+}
+
+
+module.exports = {addGroup, getGroups, getGroupID, getGroupIDbyGroupname, getGroupMembers, getGroupDetails};
--- backend/routes/group.js
+++ backend/routes/group.js
@@ -1,5 +1,5 @@
 const { group, error } = require('console');
-const { addGroup, getGroups, getGroupID, getGroupIDbyGroupname } = require('../database/group_db');
+const { addGroup, getGroups, getGroupID, getGroupIDbyGroupname, getGroupMembers, getGroupDetails } = require('../database/group_db');
 const { add2GroupChoices, getGroupChoices, deleteGroupChoice } = require('../database/groupchoices_db')
 const { makeAdmin } = require('../database/groupmember_db');
 const { getUserID } = require('../database/users_db');
@@ -85,5 +85,18 @@ router.delete('/deleteFromWatchlist', async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 });
- 
+  
+router.get('/getGroupContent', async (req, res) => {
+    try {
+        const idgroup = req.body.idgroup;
+        console.log("backend, haetaan ryhmän tietoja", idgroup);
+        const members = await getGroupMembers(idgroup);
+        const groupDetails = await getGroupDetails(idgroup);
+        const groupchoices = await getGroupChoices(idgroup);
+        res.json({members: members, groupDetails:groupDetails, groupchoices:groupchoices});
+    } catch(err) {
+        console.log("virhe koko haussa");
+        res.status(500).json({ error: err.message })
+    }
+})
 module.exports = router;
