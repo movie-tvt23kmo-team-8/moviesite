2e392d30761924612c10938e4b1ec93cb0554d5b
Ereride <143874893+Ereride@users.noreply.github.com>
2024-04-30 22:05:37 +0300

    homepage responsiivisempi

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import './home.css'
-import Header from '../components/Header'
 import PopularMoviesPanel from '../components/PopularMoviesPanel'
 import PopularSeriesPanel from '../components/PopularSeriesPanel'
+import CinemaTrailer from '../components/MovieTrailer';
 
 
 export default function Home() {
   return (
     <div className='home-container'>
       <div className='trailer-back'>
-        <header>
-          <Header />
-        </header>
+        <CinemaTrailer />
       </div>
 
       <div className='home'>
@@ -20,7 +18,6 @@ export default function Home() {
         <h3 className='home-header'>Suosituimmat sarjat</h3>
         <PopularSeriesPanel />
       </div>
-
     </div>
   )
 }
