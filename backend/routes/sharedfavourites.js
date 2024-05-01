9471ad5b477bdb18f7aba95cf6d852d039794899
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-05-01 17:42:07 +0300

    Muokattu käyttäjän poistoa
    - Poistetaan käyttäjän perustamat ryhmät
    - Tehdään arvosteluissa nimi NULL arvoon.
    - lisätty try-catch suosikkilistan hakuun, jos käyttäjä onkin jo poistanut itsensä.

--- backend/routes/sharedfavourites.js
+++ backend/routes/sharedfavourites.js
@@ -3,13 +3,16 @@ const { getFavourites } = require('../database/favourite_db');
 const { getUserIDByPasskey, getUsername } = require('../database/users_db');
 
 router.get('/', async (req, res) => {
-    const sharekey = req.query.sharekey;
+    try{const sharekey = req.query.sharekey;
     //console.log("backendissä, sharekey:",sharekey);
     const idAccount = await getUserIDByPasskey(sharekey);
     const favourites = await getFavourites(idAccount);
     const username = await getUsername(idAccount);
     //console.log("suosikit haettu, yhteensä ",favourites.length);
     res.status(200).json({ favourites: favourites, username: username });
+    }catch (err) {
+        console.error(err);
+    }
 })
 
 module.exports = router
